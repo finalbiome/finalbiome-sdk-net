7d862274330fd5f91de777fd10deada5db3e7367
Sergey Kovalev <sergey@weedydidie.com>
2022-11-03 17:01:27 +0600

    feat: generate types and storage methods

---
+++ ConsoleTest/Types/Base/WeakBoundedVec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ajuna.NetApi.Model.Types;
+using Ajuna.NetApi.Model.Types.Base;
+
+namespace Ajuna.NetApi.Model.Types.Base
+{
+    public class BoundedVec<T, S> : BaseVec<T> where T : IType, new()
+    {
+        public override string TypeName() => $"BoundedVec<{new T().TypeName()}, u32>";
+
+    }
+}
+
